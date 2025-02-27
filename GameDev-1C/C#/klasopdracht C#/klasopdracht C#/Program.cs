88e0e06da2134b56a48b5d5cc19e9e628155d736
SantinoBrink <santinodbrink@gmail.com>
2025-02-27 09:05:40 +0100

    de start van klass C progameren

---
+++ GameDev-1C/C#/klasopdracht C#/klasopdracht C#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasopdracht_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> gebruikers = new List<User>();
+            User regi = new User("regi");
+            User diego = new User("Diego");
+            gebruikers.Add(regi);
+            gebruikers.Add(diego);
+            for (int i = 0; i < gebruikers.Count; i++)
+            {
+                Console.Write(gebruikers[i].Name);
+            };
+        }
+        public class User
+        {
+            public string Name;
+            public User(string name)
+            {
+                Name = name;
+            }
+
+        };
+    }
+}
