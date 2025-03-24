cc009323b0607aad2e5bab1a005a243a88f59f24
SantinoBrink <santinodbrink@gmail.com>
2025-03-24 11:22:19 +0100

    24-3-25

--- GameDev-1C/C#/DIY Classes in classes/DIY Classes in classes/Types.cs
+++ GameDev-1C/C#/DIY Classes in classes/DIY Classes in classes/Types.cs
@@ -8,11 +8,15 @@ namespace DIY_Classes_in_classes
 {
     class Types
     {
-        public String Soort;
+        public              String      Soort;
+      //[^toeganelijkheid] [^datetype] [^variablenaam]
         public List<Subspecies> Namen = new List<Subspecies>();
+      //[^ zorg ervoor dat er een public lijst wordt gemaakt dat bestaat uit namen van subspiecies class]
         public Types(string soort)
+       //[^maakt het dat als je de types variable maakt dat je ook een soort moet toevoegen]
         {
             Soort = soort;
+            //[^zodat de variable Soort dezelde naam als soort zal hebben]
         }
     }
 }
---
+++ GameDev-1C/C#/Singletons/Singletons/Manegement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletons
+{
+    class Manager
+    {
+
+        private static Manager instance;
+        private Manager()
+        {
+
+        }
+
+        public static Manager GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new Manager();
+            }
+            return instance;
+        }
+    }
+}
