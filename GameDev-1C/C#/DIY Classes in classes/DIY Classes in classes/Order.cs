342824206de25969d82dac21ad1d556c16b77125
SantinoBrink <santinodbrink@gmail.com>
2025-03-26 18:24:52 +0100

    .

--- GameDev-1C/C#/DIY Classes in classes/DIY Classes in classes/Order.cs
+++ GameDev-1C/C#/DIY Classes in classes/DIY Classes in classes/Order.cs
@@ -9,10 +9,14 @@ namespace DIY_Classes_in_classes
     class Order
     {
         public String Ordertypes;
+        //[^ zorgt dat Ordertypes kan opgeroept worden]
         public List<Types> Sorten = new List<Types>();
+        //[^ maakt een lijst aan dat opgemaakt wordt van data van Types]
         public Order(String ordertypes)
+       //[^ maakt het dat je een string moet mee te geven dat gelijk is aan ordertypes]
         {
             Ordertypes = ordertypes;
+            //[^ Dat Ordertypes het naam van ordertypes zal hebben]
         }
     }
 }
--- GameDev-1C/C#/DIY Classes in classes/DIY Classes in classes/Program.cs
+++ GameDev-1C/C#/DIY Classes in classes/DIY Classes in classes/Program.cs
@@ -10,11 +10,14 @@ namespace DIY_Classes_in_classes
     {
         static void Main(string[] args)
         {
+            //[maakt een nieuwe lijst aan genaamd "Valka"]
             Levensvormen Valka = new Levensvormen("Valka");
 
+            //naamt een nieuwe lijst genaamd "Animalia"
             Order Dieren = new Order("Anamalia");
             Order Flora = new Order("Flora");
 
+            //maakt een nieuwe list van de naam "Mimics"
             Types Mimics = new Types("Mimus.sp");
             Types Draken = new Types("Dracoforms.sp");
             Types Wyverns = new Types("Wyrmiforms.sp");
@@ -23,19 +26,23 @@ namespace DIY_Classes_in_classes
             Types OldOak = new Types("Ginkgo.Antpuus");
             Subspecies Huntsmen = new Subspecies("M.phag");
 
+            //voegd aan de lijst Valka de lijst van Dieren
             Valka.Orders.Add(Dieren);
             Valka.Orders.Add(Flora);
+            //voegd aan de lijst van Dieren de lijst Mimics
             Dieren.Sorten.Add(Mimics);
             Dieren.Sorten.Add(Draken);
             Dieren.Sorten.Add(Wyverns);
             Flora.Sorten.Add(BlackWood);
             Flora.Sorten.Add(Rompiles);
             Flora.Sorten.Add(OldOak);
+            //voegd aan de lijst Mimics de naam van de subspecies "Huntsmen"
             Mimics.Namen.Add(Huntsmen);
 
             for(int i = 0;i < Valka.Orders.Count; i++)
-            {
+            {//[][][][][]
                 Console.WriteLine(Valka.Orders[i].Ordertypes);
+                // visualseer de namen van de Orders in Valka
                 for(int j = 0;j < Valka.Orders[i].Sorten.Count; j++)
                 {
                     Console.WriteLine(Valka.Orders[i].Sorten[j].Soort);
